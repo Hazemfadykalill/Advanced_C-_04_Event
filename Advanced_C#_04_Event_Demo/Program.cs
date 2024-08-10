911b73dd192dc08131ee3fc10f1dffd879a83766
Hazem Fady <hazemfady123@gmail.com>
2024-08-10 06:59:21 +0300

    Add project files.

---
+++ Advanced_C#_04_Event_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advanced_C__04_Event_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
